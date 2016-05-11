837130299a426d3e1534abebaf6d954b9dad851e
Kanishka Ganguly <kanishkaganguly2002@gmail.com>
2016-05-11 19:06:27 -0400

    First Commit - Learnt Not to Trust Unity

---
+++ DynamicMover.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DynamicMover : MonoBehaviour {
+
+    // Dynamic Obstacles
+    public GameObject dynamic_obstacle;
+    public Vector3 dynamic_start;
+    public Vector3 dynamic_end;
+    private float startLerp = 0.0f;
+    private float lerpIncrement = 0.01f;
+    private bool this_way = false;
+
+	// Use this for initialization
+	void Start () {
+		dynamic_obstacle.transform.position = dynamic_start;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Vector3.Distance(dynamic_obstacle.transform.position, dynamic_start) == 0 && this_way == false){
+			this_way = true;
+			startLerp = 0.0f;
+    		lerpIncrement = 0.01f;
+		}else if(Vector3.Distance(dynamic_obstacle.transform.position, dynamic_end) == 0 && this_way == true){
+			this_way = false;
+			startLerp = 0.0f;
+    		lerpIncrement = 0.01f;
+		}
+
+		if(this_way){
+			dynamic_obstacle.transform.position = Vector3.Lerp(dynamic_start, dynamic_end, startLerp);
+			startLerp += lerpIncrement;
+		}else if(!this_way){
+			dynamic_obstacle.transform.position = Vector3.Lerp(dynamic_end, dynamic_start, startLerp);
+			startLerp += lerpIncrement;
+		}
+	}
+}
---
+++ ObstacleTrigger.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleTrigger : MonoBehaviour {
+
+	public GameObject obstacle;
+	private Color originalMat;
+	private Color neighbourMat;
+
+// void OnTriggerEnter(Collider other) {
+ //        neighbourMat = Color.yellow;
+ //        obstacle.GetComponent<Renderer>().material.SetColor("_Color", neighbourMat);
+ //    }
+
+    // void OnTriggerExit(Collider other) {
+    //     originalMat = Color.green;
+    //     obstacle.GetComponent<Renderer>().material.SetColor("_Color", originalMat);
+    // }
+}
